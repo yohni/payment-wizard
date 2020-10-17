fbf2e0180b3aea05c111d099a6955c966a99f072
yohni <yohni.123@gmail.com>
2020-10-18 05:08:07 +0700

    feat: 🎸 home page

---
+++ src/views/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <Container>
+      <PaymentCard> <CheckoutDetail /> </PaymentCard>
+    </Container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Container from '@/components/layouts/MyContainer.vue';
+import PaymentCard from '@/components/PaymentCard.vue';
+import CheckoutDetail from '@/components/CheckoutDetail.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    PaymentCard,
+    Container,
+    CheckoutDetail,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../theme/breakpoints';
+</style>
