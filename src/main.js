f48c16839dee775403d109831976d00ade2e173c
yohni <yohni.123@gmail.com>
2020-10-18 05:06:31 +0700

    feat: 🎸 normalize css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import 'normalize.css';
+import '@/styles/index.styl';
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+  .use(router)
+  .mount('#app');
