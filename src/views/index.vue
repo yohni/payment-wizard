1646060ec050f01de7659b4564249bc731ba630d
yohni <yohni.123@gmail.com>
2020-10-19 22:19:38 +0700

    test: 💍 playground

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="home">
-    <Container>
-      <PaymentCard> <CheckoutDetail /> </PaymentCard>
+    <Container class="home__container">
+      <div class="home__status">
+        <Status />
+      </div>
+      <PaymentCard class="card" :state="currentState">
+        <Row class="row">
+          <div class="row__switch">
+            <CheckoutDelivery v-if="currentState === 1" />
+            <CheckoutPayment v-if="currentState === 2" />
+            <CheckoutFinish v-if="currentState === 3" />
+          </div>
+          <CheckoutSummary class="row__summary" />
+        </Row>
+      </PaymentCard>
     </Container>
   </div>
 </template>
@@ -10,18 +22,60 @@
 // @ is an alias to /src
 import Container from '@/components/layouts/MyContainer.vue';
 import PaymentCard from '@/components/PaymentCard.vue';
-import CheckoutDetail from '@/components/CheckoutDetail.vue';
+import CheckoutDelivery from '@/components/CheckoutDelivery.vue';
+import CheckoutPayment from '@/components/CheckoutPayment.vue';
+import CheckoutFinish from '@/components/CheckoutFinish.vue';
+import CheckoutSummary from '@/components/CheckoutSummary.vue';
+import Row from '@/components/layouts/MyRow.vue';
+import Status from '@/components/StateProgress.vue';
 
 export default {
   name: 'Home',
   components: {
     PaymentCard,
     Container,
-    CheckoutDetail,
+    CheckoutDelivery,
+    CheckoutPayment,
+    CheckoutSummary,
+    CheckoutFinish,
+    Row,
+    Status,
+  },
+  data() {
+    return {
+      currentState: 3,
+    };
   },
 };
 </script>
 
 <style lang="stylus" scoped>
 @import '../theme/breakpoints';
+
+.home {
+  &__container {
+    .row {
+      &__switch {
+        width: 75%;
+
+        +media-break-point-down($md) {
+          width: 100%;
+        }
+      }
+
+      &__summary {
+        width: 25%;
+
+        +media-break-point-down($md) {
+          width: 100%;
+        }
+      }
+    }
+  }
+
+  &__status {
+    display: flex;
+    justify-content: center;
+  }
+}
 </style>
