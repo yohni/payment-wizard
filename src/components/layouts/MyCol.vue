3b704b1f02855a9c986fe20ad0e8c6e4737d4d1a
yohni <yohni.123@gmail.com>
2020-10-19 22:18:49 +0700

    fix: 🐛 geo style

---
+++ src/components/layouts/MyCol.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Col',
+};
+</script>
+
+<style lang="stylus" scoped>
+.col
+  &-xl{
+    &-8{
+      
+    }
+  }
+</style>
---
+++ src/components/layouts/MyRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Row',
+};
+</script>
+
+<style lang="stylus" scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
