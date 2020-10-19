79f4265ae6414de7fce8156d3a0e4c98abda3860
yohni <yohni.123@gmail.com>
2020-10-19 22:16:40 +0700

    refactor: 💡 implement UI

---
+++ src/components/CheckoutFinish.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="finish">
+    <div class="finish__wrap">
+      <H1 class="finish__heading">Thank you</H1>
+      <div class="finish__order-id">ORDER ID: XXKYB</div>
+      <p class="finish__info">
+        Your order will be delivered today with GO-SEND
+      </p>
+      <button type="button" class="finish__btn">
+        <span class="material-icons finish__btn__icon">
+          keyboard_backspace
+        </span>
+        Go to homepage
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import H1 from '@/components/MainHeading.vue';
+
+export default {
+  name: 'CheckoutFinish',
+  components: {
+    H1,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../theme/colors';
+
+.finish {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 75vh;
+
+  &__wrap {
+    text-align: left;
+  }
+
+  &__heading {
+    margin-bottom: 24px;
+  }
+
+  &__order-id {
+    margin-bottom: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    color: $originBlack;
+  }
+
+  &__info {
+    margin-bottom: 60px;
+    font-family: Inter, sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+  }
+
+  &__btn {
+    font-family: Inter;
+    font-weight: 500;
+    font-size: 14px;
+    color: $originBlack;
+    mix-blend-mode: normal;
+    opacity: 0.6;
+    outline: none;
+    border: none;
+    background: none;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    transition: all 0.2s;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    &__icon {
+      margin-right: 4px;
+    }
+  }
+}
+</style>
---
+++ src/components/CheckoutPayment.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="payment">
+    <Row class="payment__row row">
+      <div class="payment_header row--padding-10">
+        <H1>Shipment</H1>
+      </div>
+    </Row>
+    <div class="payment__shipment payment__shipment--mb-30">
+      <FloatSelect
+        :options="shipmentOptions"
+        :selectedValue="pickedShipment"
+        @setvalue="setShipment"
+      />
+    </div>
+    <Row class="payment__row row">
+      <div class="payment_header row--padding-10">
+        <H1>Payment</H1>
+      </div>
+    </Row>
+    <div class="payment__via payment__via--mb-30">
+      <FloatSelect
+        :options="viaOptions"
+        :selectedValue="pickedVia"
+        @setvalue="setVia"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Row from '@/components/layouts/MyRow.vue';
+import H1 from '@/components/MainHeading.vue';
+import FloatSelect from '@/components/FloatSelect.vue';
+
+export default {
+  name: 'CheckoutPayment',
+  components: {
+    H1,
+    Row,
+    FloatSelect,
+  },
+  data() {
+    return {
+      shipmentOptions: [
+        {
+          id: 1,
+          name: 'GO-SEND',
+          value: 15000,
+        },
+        {
+          id: 2,
+          name: 'JNE',
+          value: 9000,
+        },
+        {
+          id: 3,
+          name: 'Personal Courier',
+          value: 29000,
+        },
+      ],
+      viaOptions: [
+        {
+          id: 1,
+          name: 'e-Wallet',
+          value: 1500000,
+        },
+        {
+          id: 2,
+          name: 'Bank Transfer',
+          value: null,
+        },
+        {
+          id: 3,
+          name: 'Virtual Account',
+          value: null,
+        },
+      ],
+      pickedShipment: '',
+      pickedVia: '',
+    };
+  },
+  methods: {
+    setShipment(selectedItem) {
+      this.pickedShipment = selectedItem;
+    },
+    setVia(selectedItem) {
+      this.pickedVia = selectedItem;
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.payment {
+  &__row .row {
+    &--padding-10 {
+      padding: 30px 10px;
+    }
+  }
+
+  &__shipment--mb-30 {
+    margin-bottom: 30px;
+  }
+
+  &__via--mb-30 {
+    margin-bottom: 30px;
+  }
+}
+</style>
